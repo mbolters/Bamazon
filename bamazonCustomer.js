b4349e06579ffa0e72227d4235623c4e29d17bbe
mbolters <mariematisans@gmail.com>
2019-10-19 22:36:12 -0500

    create readProducts function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,3 +8,29 @@ var connection = mysql.createConnection({
   password: config.password,
   database: "products_DB"
 });
+
+connection.connect(function(err) {
+    if (err) console.log (err);
+    readProducts();
+    });
+    
+    function readProducts() {
+        connection.query("SELECT * FROM products", function(err, res) {
+            for (var i = 0; i < res.length; i++) {
+                console.log(
+                    "Item ID: " +
+                    res[i].item_id +
+                    " \n Product: " +
+                    res[i].product_name +
+                    " \n Department: " +
+                    res[i].department_name +
+                    " \n Price: $" +
+                    res[i].price +
+                    " \n Stock: " +
+                    res[i].stock_quantity+
+                    "\n *******************************************"
+                );
+            }
+            chooseProduct();
+        })
+    };
