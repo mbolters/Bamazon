226ec53adc4b42e30a15b9169d755b4acd91b68b
mbolters <mariematisans@gmail.com>
2019-10-19 22:41:12 -0500

    create function to determine if there is enough stock and return response

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -58,5 +58,20 @@ connection.connect(function(err) {
                 return false;
             }
         }]).then(function(answer){
+                var query = "Select product_name, stock_quantity, price, department_name FROM products WHERE ?";
+                connection.query(query, { item_id: answer.productID}, function(err, res) {
+                    if (err) console.log(err);
 
-        })
+                    var available_stock = res[0].stock_quantity;
+                    var price_per_unit = res[0].price;
+                    var productDepartment = res[0].department_name;
+
+                    if (available_stock >= answer.stock) {
+                        completePurchase(available_stock, price_per_unit, productDepartment, answer.productID, answer.stock);
+                    } else {
+                        console.log("Insufficient stock!");
+                        chooseProduct();
+                    }
+                });
+            });
+    };
